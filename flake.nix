a1be78184434b34d382fc4a44cce6ff1751596f6
Austin Horstman <khaneliman12@gmail.com>
2024-03-07 15:47:35 -0600

    flake: init

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      genSystems = func: lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ]
+        (system: func (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = genSystems
+        (pkgs:
+          {
+            default =
+              pkgs.mkShell
+                {
+                  name = "month-of-ai-shell";
+
+                  buildInputs = with pkgs; [
+                    dotnetCorePackages.dotnet_8.sdk
+                  ];
+
+                  nativeBuildInputs = with pkgs;
+                    [
+                    ];
+                };
+          });
+    };
+}
