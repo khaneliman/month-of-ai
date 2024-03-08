ba4e43c3bffec775d226daa1724d1199a343c408
Austin Horstman <khaneliman12@gmail.com>
2024-03-08 15:27:32 -0600

    flake.nix: update

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,51 +1,40 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      inherit (nixpkgs) lib;
-      genSystems = func: lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ]
-        (system: func (import nixpkgs { inherit system; }));
-    in
-    {
-      devShells = genSystems
-        (pkgs:
-          {
-            default =
-              pkgs.mkShell
-                {
-                  name = "month-of-ai-shell";
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          name = "month-of-ai-shell";
 
-                  buildInputs = with pkgs; [
-                    cargo
-                    rustc
-                    rust-analyzer
-                    rustfmt
-                    clippy
-                    openssl
-                  ]
-                  ++ lib.optionals stdenv.isDarwin
-                    (with pkgs.darwin.apple_sdk.frameworks;
-                    [
-                      SystemConfiguration
-                    ]);
+          buildInputs = with pkgs; [
+            cargo
+            rust-bin.stable.latest.default
+            rustfmt
+            clippy
+            openssl
+          ]
+          ++ lib.optionals stdenv.isDarwin
+            (with pkgs.darwin.apple_sdk.frameworks;
+            [
+              SystemConfiguration
+            ]);
 
-                  nativeBuildInputs = with pkgs;
-                    [
-                      pkg-config
-                    ];
-                };
-          });
-    };
+          nativeBuildInputs = with pkgs;
+            [
+              pkg-config
+            ];
+        };
+      });
 }
