f016790bad81d52e0bac1f86158ac2b75ac31d3d
Austin Horstman <khaneliman12@gmail.com>
2024-03-20 20:29:00 -0500

    movie.rs: add MovieCriteria

--- rust/src/model/movie.rs
+++ rust/src/model/movie.rs
@@ -32,3 +32,35 @@ struct Cast {
     character: String,
     profile_path: String,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MovieCriteria {
+    pub search: Option<String>,
+    pub genre: Option<String>,
+    pub mpaa: Option<String>,
+    pub release_date_min: Option<String>,
+    pub release_date_max: Option<String>,
+    pub score_min: Option<f32>,
+    pub score_max: Option<f32>,
+    pub natural_language: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SortCriteria {
+    pub field: String,
+    pub direction: Option<SortDirection>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum SortDirection {
+    Asc,
+    Desc,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Criteria {
+    pub sort: Option<Vec<SortCriteria>>,
+    pub page: Option<i32>,
+    pub page_size: Option<i32>,
+}
