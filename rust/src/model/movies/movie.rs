b8f7a60e475f46018d5d8682aa06756ecc496699
Austin Horstman <khaneliman12@gmail.com>
2024-03-30 13:38:00 -0500

    movie: add TopRatedMovie since id type is different

--- rust/src/model/movies/movie.rs
+++ rust/src/model/movies/movie.rs
@@ -5,7 +5,7 @@ pub struct Movie {
     #[allow(dead_code)]
     backdrop_path: String,
     #[allow(dead_code)]
-    id: String,
+    pub id: String,
     #[allow(dead_code)]
     title: String,
     #[allow(dead_code)]
@@ -44,6 +44,54 @@ pub struct Movie {
     summaries: Vec<String>,
     #[allow(dead_code)]
     synopsis: String,
+    #[allow(dead_code)]
+    imdb_score: f64,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct TopRatedMovie {
+    #[allow(dead_code)]
+    backdrop_path: String,
+    #[allow(dead_code)]
+    pub id: i32,
+    #[allow(dead_code)]
+    title: String,
+    #[allow(dead_code)]
+    poster_path: String,
+    #[allow(dead_code)]
+    release_date: String,
+    #[allow(dead_code)]
+    vote_average: f64,
+    #[allow(dead_code)]
+    vote_count: u32,
+    #[allow(dead_code)]
+    popularity: f64,
+    #[allow(dead_code)]
+    pub overview: Option<String>,
+    #[allow(dead_code)]
+    imdb_id: Option<String>,
+    #[allow(dead_code)]
+    budget: Option<u32>,
+    #[allow(dead_code)]
+    homepage: Option<String>,
+    #[allow(dead_code)]
+    revenue: Option<u32>,
+    #[allow(dead_code)]
+    runtime: u32,
+    #[allow(dead_code)]
+    pub tagline: Option<String>,
+    #[allow(dead_code)]
+    pub genres: Vec<String>,
+    #[allow(dead_code)]
+    cast: Option<Vec<Cast>>,
+    #[allow(dead_code)]
+    pub keywords: Option<Vec<String>>,
+    #[allow(dead_code)]
+    mpaa: String,
+    #[allow(dead_code)]
+    pub summaries: Option<Vec<String>>,
+    #[allow(dead_code)]
+    pub synopsis: Option<String>,
     #[allow(dead_code)]
     imdb_score: f64,
 }
