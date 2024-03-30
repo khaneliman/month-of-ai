af80b17bd71178af6a88713b206733f30492b8ea
Austin Horstman <khaneliman12@gmail.com>
2024-03-30 16:20:54 -0500

    cosine_similarity: add model

---
+++ rust/src/model/cosine_similarity.rs
@@ -0,0 +1,5 @@
+#[derive(Debug, Clone)]
+pub struct CosineSimilarity {
+    pub movie_id: i32,
+    pub similarity: f32,
+}
--- rust/src/model/mod.rs
+++ rust/src/model/mod.rs
@@ -1,6 +1,7 @@
 pub mod chat_completion_request;
 pub mod chat_completion_response;
 pub mod config;
+pub mod cosine_similarity;
 pub mod embedding_request_body;
 pub mod movies;
 pub mod query;
